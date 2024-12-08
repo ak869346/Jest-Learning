055a827ccc75697c4dcd8f9be39df8c81933ea4e
Ankit Kumar <jiraankit@gmail.com>
2024-12-08 19:54:32 +0530

    Adding basic test cases

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,24 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('Check Ankit TestCase', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Testing with Ankit/i);
+  const title = screen.getByTitle("Testing image");
+  expect(linkElement).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+});
+
+test("Testing Input Box",()=> {
+  render(<App/>);
+  let checkInput = screen.getByRole("textbox");
+  let checkInputPlaceHolder = screen.getByPlaceholderText("Enter user name");
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceHolder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name","username");
+  expect(checkInput).toHaveAttribute("id","userid");
+})
+
+
